f66273f2e68d8060c18800007065d40a4a6778e9
Sakamoto-san <skmt3p@gmail.com>
2020-01-27 18:28:00 +0900

    [add]  add gatsby-plugin-no-sourcemaps

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,7 @@ module.exports = {
         icon: `src/images/prof_real_mix icon.png`, // This path is relative to the root of the site.
       },
     },
+    'gatsby-plugin-no-sourcemaps',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
