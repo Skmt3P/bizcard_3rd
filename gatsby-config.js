c33a2c42066b6cfadc7a7dad57609d9d44cea227
Sakamoto-san <skmt3p@gmail.com>
2020-01-27 17:31:37 +0900

    [add] init gatsby pj

---
+++ gatsby-config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  siteMetadata: {
+    title: `R.Sakamoto's BizCard`,
+    description: `R.Sakamoto(@Skmt3P) is a President of OmusBridge OÜ | Front-end Engineer | Teacher  `,
+    author: `@skmt3p`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Skmt3P's BizCard`,
+        short_name: `Skmt3P's`,
+        start_url: `/`,
+        background_color: `#BC002D`,
+        theme_color: `#BC002D`,
+        display: `minimal-ui`,
+        icon: `src/images/prof_real_mix icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+import Image from '../components/image'
+import SEO from '../components/seo'
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1> This page is under construction! </h1>{' '}
+    <p>
+      Please visit{' '}
+      <a href="https://2019.skmt3p.com" target="_blank">
+        {' '}
+        here{' '}
+      </a>
+      !{' '}
+    </p>{' '}
+    <div
+      style={{
+        maxWidth: `300px`,
+        marginBottom: `1.45rem`,
+      }}>
+      <Image />
+    </div>{' '}
+  </Layout>
+)
+
+export default IndexPage
