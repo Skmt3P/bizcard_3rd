184210ed04155847a988c41041698c951096463a
Sakamoto-san <skmt3p@gmail.com>
2020-01-31 20:29:01 +0900

    fix import2

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,7 @@
 import 'ress'
-
-export const onClientEntry = async () => {
-    if (typeof IntersectionObserver === `undefined`) {
-        await import(`intersection-observer`);
-    }
-    if (typeof ResizeObserver === `undefined`) {
-        await import(`resize-observer-polyfill`);
-    }
+if (typeof IntersectionObserver === `undefined`) {
+  await import(`intersection-observer`)
+}
+if (typeof ResizeObserver === `undefined`) {
+  await import(`resize-observer-polyfill`)
 }
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,3 +3,9 @@
  *
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
+if (typeof IntersectionObserver === `undefined`) {
+    await import(`intersection-observer`);
+}
+if (typeof ResizeObserver === `undefined`) {
+    await import(`resize-observer-polyfill`);
+}
